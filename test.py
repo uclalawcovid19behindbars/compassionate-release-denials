70aa7e7f04cb94f78d37965bc3c6b25dfeefcce5
hjohns12 <hopecaneel@gmail.com>
2020-10-29 17:08:27 -0400

    add slightly more complex test file

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-# import pandas as pd
+import pandas as pd
 
-# d = {'col1': [1, 2], 'col2': [3, 4]}
-# df = pd.DataFrame(data=d)
-# df
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+print("a dataframe!", df)
 
-thing = 2+2
-print("thing!", thing)
