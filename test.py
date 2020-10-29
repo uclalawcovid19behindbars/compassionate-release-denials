fddbf6ae798c9f3a7ea78f9c8284ec02a7360b89
hjohns12 <hopecaneel@gmail.com>
2020-10-29 15:38:55 -0400

    update test to make it EVEN simpler

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import pandas as pd
+# import pandas as pd
 
-d = {'col1': [1, 2], 'col2': [3, 4]}
-df = pd.DataFrame(data=d)
-df
+# d = {'col1': [1, 2], 'col2': [3, 4]}
+# df = pd.DataFrame(data=d)
+# df
+
+thing = 2+2
+print("thing!", thing)
