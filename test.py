832462c54d3b026499dfc95dce4926d76a2207ac
hjohns12 <hopecaneel@gmail.com>
2020-12-15 09:14:59 -0500

    fix: move test file

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ d = {'col1': [1, 2], 'col2': [3, 4]}
 df = pd.DataFrame(data=d)
 
 def return_data(dat):
-    return(dat)
+    return("column 1", dat['col1'], "column 2", dat['col2'])
 
 return_data(df)
