373652b344e214de712c6c866c5f79b688716368
hjohns12 <hopecaneel@gmail.com>
2021-04-26 11:43:53 -0400

    rm unnecessary scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-# fetch all data from courtlistener
-python get-cases.py
-python clean_archived_dat.py
-
-# filter and update data
-Rscript filter-newest-data.r
-
-# post data on google sheet in a new tab
